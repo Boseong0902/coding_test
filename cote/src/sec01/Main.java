654ef50476341f555e591078687a7f4f10e5d83f
Boseong@0902 <alskdj7879@gmail.com>
2025-09-06 23:29:18 +0900

    아직 기본 문법

--- cote/src/sec01/Main.java
+++ cote/src/sec01/Main.java
@@ -1,49 +1,16 @@
 package sec01;
 
-class human{
-    String name;
-    int age;
-    human(String name, int age){
-        this.name = name;
-        this.age = age;
+class A{
+    int n = 1;
+    @Override
+    public String toString() {
+        return "필드값 n = " + n;
     }
-    void eat(){}
-    void sleep(){}
-}
-
-class student extends human{
-    int studentID;
-    student(String name, int age, int studentId){
-        super(name, age);
-        this.studentID = studentId;
-    }
-    void study(){}
-}
-
-class worker extends human{
-    int workerID;
-    worker(String name, int age, int workerID){
-        super(name, age);
-        this.workerID = workerID;
-    }
-    void work() {}
 }
 
 public class Main{
-    public static void main(String[] args)
-    {
-        human h = new human("자바", 11);
-        h.eat();
-        h.sleep();
-
-        student s = new student("학생자바", 12, 3);
-        s.eat();
-        s.sleep();
-        s.study();
-
-        worker w = new worker("사원자바", 13, 4);
-        w.eat();
-        w.sleep();
-        w.work();
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a);
     }
 }
