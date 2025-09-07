d14e4bfc2d16ff2a33f113f9cc10cab166db799d
Boseong@0902 <alskdj7879@gmail.com>
2025-09-07 17:26:34 +0900

    key-value 제네릭 객체 만들어봄(key를 객체로 받는)

--- cote/src/sec01/Main.java
+++ cote/src/sec01/Main.java
@@ -1,16 +1,33 @@
 package sec01;
 
-class A{
-    int n = 1;
-    @Override
-    public String toString() {
-        return "필드값 n = " + n;
+class keyValue<k, v>{
+    private k key;
+    private v value;
+
+    public keyValue(k key, v value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public v getValue(k key){
+        if(key == this.key){
+            return this.value;
+        }
+        return null;
     }
 }
 
+class key{
+}
+
 public class Main{
-    public static void main(String[] args) {
-        A a = new A();
-        System.out.println(a);
+    public static void main(String[] args){
+
+        key k1 = new key();
+        key k2 = new key();
+
+        keyValue<key, String> kv = new keyValue<>(k1, "hi");
+        System.out.println(kv.getValue(k1));
+        System.out.println(kv.getValue(k2));
     }
 }
