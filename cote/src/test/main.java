445858f74746c8c16cf295a163b904cb6a4b4b8d
Boseong@0902 <alskdj7879@gmail.com>
2025-09-17 17:38:41 +0900

    set<E> - hashset<E>기본 예제 코드

--- cote/src/test/main.java
+++ cote/src/test/main.java
@@ -1,7 +1,24 @@
 package test;
 
+import java.util.*;
+
 public class main{
     public static void main(String[] args){
+        Set<String> hset1 = new HashSet<>();
 
+        hset1.add("1");
+        hset1.add("2");
+        hset1.add("3");
+        hset1.add("1");
+        System.out.println(hset1);
+
+        Set<String> hset2 = new HashSet<>();
+        hset2.add("1");
+        hset2.add("2");
+        hset2.add("3");
+
+        hset1.addAll(hset2);
+
+        System.out.println(hset1);
     }
 }
