f812003a5897b02b46b2a44e16cfa5d12a728652
Boseong@0902 <alskdj7879@gmail.com>
2025-09-17 18:11:04 +0900

    for-each vs iterator

--- cote/src/test/main.java
+++ cote/src/test/main.java
@@ -4,21 +4,29 @@ import java.util.*;
 
 public class main{
     public static void main(String[] args){
-        Set<String> hset1 = new HashSet<>();
+        Set<String> hset = new HashSet<>();
 
-        hset1.add("1");
-        hset1.add("2");
-        hset1.add("3");
-        hset1.add("1");
-        System.out.println(hset1);
+        hset.add("1");
+        hset.add("2");
+        hset.add("3");
 
-        Set<String> hset2 = new HashSet<>();
-        hset2.add("1");
-        hset2.add("2");
-        hset2.add("3");
+        for(String i:hset){
+            System.out.print(i);
+//            if(i == "1"){
+//                System.out.remove(i);
+//            }
+        }
+        System.out.println();
 
-        hset1.addAll(hset2);
-
-        System.out.println(hset1);
+        Iterator<String> it = hset.iterator();
+        while(it.hasNext()){
+            String val = it.next();
+            System.out.print(val);
+            if(val=="1"){
+                it.remove();
+            }
+        }
+        System.out.println();
+        System.out.print(hset);
     }
 }
