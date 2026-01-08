09c991f1ca16c3a4dc1053e20efe9218b56af736
박보성 <bagboseong@bagboseong-ui-MacBookPro.local>
2026-01-08 23:40:00 +0900

    26_1_8

---
+++ cote/src/sec103/Main.java
@@ -0,0 +1,18 @@
+package sec103;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int max = 0;
+        String answer=null;
+        for(String s:in.nextLine().split(" ")){
+            if(s.length()>max){
+                max=s.length();
+                answer=s;
+            }
+        }
+        System.out.println(answer);
+    }
+}
