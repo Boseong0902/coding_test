7ba3f69f33c11ef3ab83a5203bf76c0857b72d44
박보성 <bagboseong@bagboseong-ui-MacBookPro.local>
2026-01-09 12:04:42 +0900

    26_1_9

---
+++ cote/src/sec104/Main.java
@@ -0,0 +1,22 @@
+package sec104;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        Deque<Character> text =  new ArrayDeque<>();
+        int n = in.nextInt();
+        for(int i = 0; i<n; i++){
+            String str = in.next();
+            char[] ch = str.toCharArray();
+            for(char c : ch){
+                text.offerFirst(c);
+            }
+            for(char c : text){
+                System.out.print(text.pop());
+            }
+            System.out.println();
+        }
+    }
+}
