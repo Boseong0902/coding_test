cf64a59e8a7e418c8b864d652b914dcd871be917
박보성 <alskdj7879@gmail.com>
2026-01-21 14:36:53 +0900

    26.1.20

--- cote/src/sec204/mine/Main.java
+++ cote/src/sec204/mine/Main.java
@@ -15,11 +15,32 @@ public class Main{
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        long startTime;
+        long endTime;
+
+        // 재귀 사용
         int n = sc.nextInt();
+        startTime = System.nanoTime();
         int a = 1;
         int b = 1;
         System.out.print(a + " " + b + " ");
         fib(a,b,n-2);
+        endTime = System.nanoTime();
+        System.out.println("재귀: "+(endTime-startTime));
+
+        //#######################################################
+        // 단순 반복문 사용
+        startTime = System.nanoTime();
+        int c = 1, d = 1, e;
+        System.out.print(c + " " + d + " ");
+        for(int i = 2; i<n; i++){
+            e = c+d;
+            System.out.print(e+" ");
+            c = d;
+            d = e;
+        }
+        endTime = System.nanoTime();
+        System.out.println("단순 반복문: "+(endTime-startTime));
 
     }
 }
