a9cde0dacbed3036d103f4eaa17a4b858a933db4
박보성 <alskdj7879@gmail.com>
2026-01-22 15:50:51 +0900

    26.1.22

---
+++ cote/src/sec504/mine/Main.java
@@ -0,0 +1,39 @@
+package sec504.mine;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char[] list =  sc.nextLine().toCharArray();
+        Deque<Integer> stack=new ArrayDeque<>();
+
+        for(char c : list){
+            if(Character.isDigit(c)){
+                stack.offerLast(Character.getNumericValue(c));
+            }
+            else{
+                int num2 = stack.pollLast();
+                int num1 = stack.pollLast();
+                if(c=='+'){
+                    int num =  num1+num2;
+                    stack.offerLast(num);
+                }
+                else if(c=='-'){
+                    int num =  num1-num2;
+                    stack.offerLast(num);
+                }
+                else if(c=='*'){
+                    int num =  num1*num2;
+                    stack.offerLast(num);
+                }
+                else if(c=='/'){
+                    int num =  num1/num2;
+                    stack.offerLast(num);
+                }
+            }
+        }
+        System.out.println(stack.pollLast());
+    }
+}
+
+//c-'0'
