3f9a908a80ab0cde1f9a8d08aacdb1c06e973367
박보성 <alskdj7879@gmail.com>
2026-02-16 13:45:58 +0900

    26.2.16

--- cote/src/sec814/mine/Main.java
+++ cote/src/sec814/mine/Main.java
@@ -1,6 +1,6 @@
 // 전략
 // 각 피자집들의 포함 여부를 dfs(재귀함수)를 사용하여 모든 경우의 수를 따진다. -> 각 피자집들의 포함 여부를 반영한 전체 지도 완성
-//      근데 여기서 combination을 고려해야될 듯. n개의 피자집 중 r개를 뽑는 모든 경우를 탐색하는거니까
+//      근데 여기서 dfs를 combination으로 구현하는걸 고려해야될 듯. n개의 피자집 중 r개를 뽑는 모든 경우를 탐색하는거니까
 // 각 리프루프에서 bfs(큐 사용)하여 도시의 최소 피자집 거리를 구한다
 // static변수로 min선언하고, 각 루프마다 최소값 갱신여부 확인 후 갱신 수행
 // 모든 재귀 루프가 끝나면 min값 반환
@@ -12,9 +12,14 @@ package sec814.mine;
 
 import java.util.*;
 public class Main{
+    static int n;
     static int min = 1000;
     static int originMaps[][];
-    static void bfs(Deque<int[]> house, int maps[][]){
+    static int[] pizzaLoc[];
+    static int houseLoc[][];
+    static int limit; // 제외될 피자 집 개수
+
+    static void bfs(Deque<int[]> house, int[][]maps){
         int sum = 0;
         int dist = 0;
         while(!house.isEmpty()){
@@ -46,13 +51,58 @@ public class Main{
         }
         if(sum < min){
             min = sum;
+
         }
     }
+    static void dfs(int currentCnt, int[] deadPizza[], int idx){
+        if(currentCnt==limit){
+            Deque<int[]> house = new ArrayDeque<>();
+            int[][] copyMaps = new int[n][n];
 
-    static void dfs(){
-
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<n; j++){
+                    copyMaps[j][i] = originMaps[j][i];
+                    if(originMaps[j][i] == 1){
+                        int[]put = {j,i};
+                        house.offerLast(put);
+                    }
+                }
+            }
+            for(int i = 0; i<deadPizza.length; i++){
+                int x = deadPizza[i][0];
+                int y = deadPizza[i][1];
+                copyMaps[x][y] = 3;
+            }
+            bfs(house, copyMaps);
+            return;
+        }
+        deadPizza[currentCnt] = pizzaLoc[idx];
+        for(int i = 0; i<pizzaLoc.length; i++){
+            dfs(currentCnt+1, deadPizza, idx+1+i);
+        }
     }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int pizzaCnt = 0;
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                int k  = sc.nextInt();
+                originMaps[j][i] = k;
+                if(k == 2){
+                    pizzaCnt++;
+                }
+            }
+        }
+        limit = pizzaCnt-m;
+        int[] deadPizza[] = new int[limit][2];
+        for(int i = 0; i<pizzaLoc.length; i++){
+            dfs(0, deadPizza, i);
+        }
+
+        System.out.print(min);
     }
 }
--- cote/src/test/Main.java
+++ cote/src/test/Main.java
@@ -1,20 +1,13 @@
-package test;
-
-import java.util.*;
-
-public class Main{
-    public static void main(String[] args){
-        String a = "1234";
-        char b[] = a.toCharArray();
-        int c = 0;
-        for(int i = 0; i < b.length; i++){
-            System.out.print(b[i]+" ");
-            int d = Character.getNumericValue(b[i]);
-            c+=(d*10);
-            System.out.println(c);
-        }
-
-    }
-}
-
-//이렇게 수정해도 반영 안되는거지
+//package test;
+//
+//import java.util.*;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        int[] test[] = new int[4][2];
+//        test[0] = {1,2};
+//
+//    }
+//}
+//
+////이렇게 수정해도 반영 안되는거지
